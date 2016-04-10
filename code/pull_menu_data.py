7c2b73e83e2787175290cdf61ef75f13918db0e1
eronning <erik_ronning@brown.edu>
2016-04-09 23:25:45 -0400

    added a script that pulls a year of menu data from the ratty

--- code/pull_menu_data.py
+++ code/pull_menu_data.py
@@ -7,25 +7,32 @@ import urllib2
  
 
 api_key = "a5fe3ea6-fcdc-4cfa-b8de-092afcb7edd0"
+yearly = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+years = [2015, 2016, 2017]
+
+data = []
 
 print "Fetching menu data"
-try:
-	# initialise your csv file
-	with open('menu_data.csv', 'wb') as outfile:
-		writer = csv.writer(outfile)
-		headers = [] # edit these as required
-		writer.writerow(headers)
 
-		# enter the first and last day required here
-		# start_date = datetime.date(2015,04,7)
-		# end_date = datetime.date(2016,04,7)
-		url = ("https://api.students.brown.edu/dining/menu?client_id=%s&eatery=ratty" %
-					(api_key))
-		data = json.load(urllib2.urlopen(url))
-		# data = requests.get(url).json()
-		print data
+# initialise your csv file
 
-except Exception:
-	os.remove(outfile)
+with open('menu_data.csv', 'wb') as outfile:
+	writer = csv.writer(outfile)
+	headers = [] # edit these as required
+	writer.writerow(headers)
+	for year in years:
+		for month in yearly:
+			for day in range(yearly[month]):
+				# don't go past the current day -- there will be no menu data
+				if year is 2015 and month is 4 and day is 8:
+					break
+				url = ("https://api.students.brown.edu/dining/menu?client_id=%s&eatery=ratty&year=%s&month=%s&day=%s" %
+						(api_key, year, month, day + 1))
+				try:
+					data.append(json.load(urllib2.urlopen(url)))
+					print url 
+				except Exception:
+					print "No menu data for %s, %s, %s" % (month, day, year)
+
 
 print "Done!"
