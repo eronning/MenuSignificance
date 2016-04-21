93bc6efc120b6c865c6ff4673d97421b7cae3b84
eronning <erik_ronning@brown.edu>
2016-04-20 21:39:12 -0400

    Finished pulling course data from api. The necessary data is then written to a csv file

--- code/course/pull_course_data.py
+++ code/course/pull_course_data.py
@@ -9,9 +9,8 @@ from collections import defaultdict
 api_key = "a5fe3ea6-fcdc-4cfa-b8de-092afcb7edd0"
 
 print "Fetching course data"
-courses = "CSCI1380-S01,CSCI1670-S01,CSCI0160"
 departments = "CSCI"
-semesters = []
+semesters = ["Fall%202015", "Spring%202016"]
 # initialise your csv file
 course_data = {}
 department_data = []
@@ -29,43 +28,46 @@ with open('course_data.csv', 'wb') as outfile:
 	except Exception:
 		print "Error pulling departments"
 
-	
-	for code in department_data:	
-		url = ("https://api.students.brown.edu/academic/departments/%s?client_id=%s" %
-			(code, api_key))
-		# try:
+	for semester in semesters:
+		# gather the whole semesters info
+		print "fetching %s semester data" % (semester)
+		for code in department_data:	
+			url = ("https://api.students.brown.edu/academic/departments/%s?client_id=%s&semester=%s" %
+				(code, api_key, semester))
 			# gather all the info for that department
-		total_info = []
-		curr_info = json.load(urllib2.urlopen(url))
-		total_info.append(curr_info)
-		next_url = curr_info['next']
-		# gather each page of information
-		# while next_url != 'null':
-		# 	curr_info = json.load(urllib2.urlopen(url))
-		# 	total_info.append(curr_info)
-		# 	next_url = curr_info['next']
-		# aggregate all the information for that department
-		for info in total_info:
-			info_items = info['items']
-			for item in info_items:
-				meeting = item['meeting']
-				# check if there is a meeting time				
-				if meeting:
-					try:
-						# take the first meeting time (in case there are multiple)
-						meeting = meeting[0]
-						# get the row information
-						row = [item['crn'], item['full_number'], item['number'], item['title'], item['seats_available'], item['seats_total'], meeting['location'], meeting['start_time'], meeting['end_time']]
-						writer.writerow(row)
-						print "added course %s %s" % (item['number'], item['title'])
-					except Exception:
-						print "excluded invalid course (not enough information %s)" % (item['number'])
+			print "fetching %s department data" % (code)
+			try:
+				total_info = []
+				curr_info = json.load(urllib2.urlopen(url))
+				total_info.append(curr_info)
+				next_url = curr_info['next']
+				# gather each page of information
+				while next_url != 'null':
+					curr_info = json.load(urllib2.urlopen(next_url))
+					total_info.append(curr_info)
+					next_url = curr_info['next']
+				# aggregate all the information for that department
+				for info in total_info:
+					info_items = info['items']
+					for item in info_items:
+						meeting = item['meeting']
+						# check if there is a meeting time				
+						if meeting:
+							try:
+								# take the first meeting time (in case there are multiple)
+								meeting = meeting[0]
+								# get the row information
+								row = [item['crn'], item['full_number'], item['number'], item['title'], item['seats_available'], item['seats_total'], meeting['location'], meeting['start_time'], meeting['end_time']]
+								writer.writerow(row)
+								print "added course %s %s" % (item['number'], item['title'])
+							except Exception:
+								print "excluded invalid course (not enough information %s)" % (item['number'])
 					
 					
 					
 
-		# except Exception:
-		# 	print "Error pulling %s course information" % (code)
+			except Exception:
+				print "Error pulling course information %s for %s" % (code, semester)
 
 
 print "Done!"
